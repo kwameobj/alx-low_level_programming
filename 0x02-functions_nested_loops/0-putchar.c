fdad8b64d391add00c2e2c1b72f3951176d0798b
kowusubamfo <kowusubamfo@gmail.com>
2023-02-22 03:05:19 +0000

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,18 +5,18 @@
  * Return: Always 0 (success)
  */
 int main(void)
-	{
+{
 		char str[] = "_putchar\n";
 
 		int i = 0;
 
 		while (str[i] != '\0')
+		{
+			char c = str[i];
 
-		char c = str[i];
-
-		_putchar(c);
-
-		i++;
-	}
+			_putchar(c);
+		
+			i++;
+		}
 return (0);
 }
