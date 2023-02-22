701748225ee78641baad12499f55462bf4b1717a
kowusubamfo <kowusubamfo@gmail.com>
2023-02-22 04:51:18 +0000

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_sign - tells whether a number is negative or positive
+ * @n: the number
+ * Description: prints the negative or positive status of a number
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+_putchar('0');
+return (0);
+}
