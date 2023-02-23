dd6da34ff8cf00b228068f2d587d0298c6729b3e
kowusubamfo <kowusubamfo@gmail.com>
2023-02-23 16:41:16 +0000

    Task 4

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - takes a date and prints remaining days in the year
+ * Return: 0
+ */
+
+int main (void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 2;
+	day = 29;
+	year = 2000;
+
+	printf("Date: %02d/%2d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}
