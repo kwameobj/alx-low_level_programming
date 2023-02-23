eaf48efd25a9b778856b319adffe2b0201cba420
kowusubamfo <kowusubamfo@gmail.com>
2023-02-23 12:31:40 +0000

    Task 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,25 +8,24 @@
  * Return: 0
  */
 
-int main(void)
+int positive_or_negative(int i)
 {
-	int n;
+	int i;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
-	else if (n == 0)
+	else if (i == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
-	else if (n < 0)
+	else if (i < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
-	return (0);
 }
