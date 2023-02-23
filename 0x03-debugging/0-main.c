7e9f159ed89984b65f8b9767c4bea7ddf6a86e73
kowusubamfo <kowusubamfo@gmail.com>
2023-02-23 12:39:03 +0000

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,25 @@
-sdvdfvs
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description: prints  if a number is positive or negative.
+ * Return: 0
+ */
+
+void positive_or_negative (int i)
+{
+        if (i > 0)
+        {
+                printf("%d is positive\n", i);
+        }
+        else if (i == 0)
+        {
+                printf("%d is zero\n", i);
+        }
+        else if (i < 0)
+        {
+                printf("%d is negative\n", i);
+        }
+}
