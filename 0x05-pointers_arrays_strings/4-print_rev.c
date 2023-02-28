d1068c4e679f4a6817174920d3f1fd368d405173
kowusubamfo <kowusubamfo@gmail.com>
2023-02-28 02:52:05 +0000

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: given string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
