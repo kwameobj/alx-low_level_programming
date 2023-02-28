87808f73f0bb9e32971d467c032873b720103acd
kowusubamfo <kowusubamfo@gmail.com>
2023-02-28 00:59:30 +0000

    Task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,9 @@
 /**
 * reset_to_98 -a function that takes a pointer to an int
 * @n: puntero
+*
+* Description: a function that takes a pointer to an int as parameter
+* and updates the value it points to to 98.
 * Return: 0
 */
 
