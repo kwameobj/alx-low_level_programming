76a17bbd801896ba358109846f7323c81ea8e895
kowusubamfo <kowusubamfo@gmail.com>
2023-03-03 01:29:43 +0000

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,28 @@
  * _strcat - concatenates two strings
  * @dest: first string
  * @src: second string
+ * @n: number of elements in src array
  * Description: a function that puts two strings together
  * using 'n' bytes to modify src
  * Return: 0
 */
+
+char *_strncat(char *dest, char *src, int n);
+
+{
+
+int j;
+j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
