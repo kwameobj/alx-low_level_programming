9a9febe4944a9619565a3892548e9728d86a7d1f
kowusubamfo <kowusubamfo@gmail.com>
2023-03-03 03:14:01 +0000

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: given string
+ * @s2: given string
+ * Description - a function that compares two strings
+ * Return: 
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
+		i++;
+
+	return (s1[i] - s2[i]);
+}
