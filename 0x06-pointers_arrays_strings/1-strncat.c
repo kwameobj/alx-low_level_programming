95c75e8c62f7bb79c8197ce428be616de561583a
kowusubamfo <kowusubamfo@gmail.com>
2023-03-03 00:50:22 +0000

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * Description: a dunction that puts two strings together
+ * eliminating the \0
+ * Return: 0
+*/
