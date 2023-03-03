67c68af5eb8aa7a5ed708385a1fe65cc4db101fa
kowusubamfo <kowusubamfo@gmail.com>
2023-03-03 04:22:03 +0000

    Task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encrypts a given string with ROT13 substituttion cipher
+ * @s: given string 
+ * Return: char value
+ */
+
+char *rot13(char *s)
+{
+	char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+		for (j = 0; part1[j] != '\0'; j++)
+		{
+			if (s[i] == part1[j])
+			{
+				s[i] = part2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
