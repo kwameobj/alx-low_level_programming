4982bff17d1c5f09c38080ac9c21001a97b1e122
kowusubamfo <kowusubamfo@gmail.com>
2023-03-03 03:44:49 +0000

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase to uppercase
+ * @s: character to make upper
+ * Return: return value of uppercase string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+
+	return (s);
+}
