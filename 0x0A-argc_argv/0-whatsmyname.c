b2ed23531f51ab35bd14d1cffae097bfc3787d7e
kowusubamfo <kowusubamfo@gmail.com>
2023-03-10 21:42:50 +0000

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * 
+ * Description: a function to print program name
+ * return: 0
+*/
+
 int main(__attribute__((unused))int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
