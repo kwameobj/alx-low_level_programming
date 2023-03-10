9d4651261a4cbfc0aa489ccffb5cc1e1ecc2d7cd
kowusubamfo <kowusubamfo@gmail.com>
2023-03-10 15:41:56 +0000

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,6 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
+
 /**
  *_memset - fill a block of memory with a specific value
  *@s: starting address of memory to be filled
@@ -7,6 +9,7 @@
  *
  *Return: changed array with new value for n bytes
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
