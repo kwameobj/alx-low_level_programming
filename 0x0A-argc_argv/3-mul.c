e1b4e0ebd226a0f5b3564af238d9f1353f2ba275
kowusubamfo <kowusubamfo@gmail.com>
2023-03-10 23:35:48 +0000

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of items on command line
+ * @argv: array of characters on command line
+ * Description - a program that multiplies two numbers.
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = atoi(argv[1]);
+	int j = atoi(argv[2]);
+	int prod = i * j;
+
+	if (argc == 3)
+	{
+		printf("%d\n", prod);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
