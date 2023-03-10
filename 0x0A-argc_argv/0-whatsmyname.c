18945535d671e2049fd92864b09a0e72f97c596e
kowusubamfo <kowusubamfo@gmail.com>
2023-03-10 21:39:21 +0000

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
-    int i = 0;
-    for(i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
