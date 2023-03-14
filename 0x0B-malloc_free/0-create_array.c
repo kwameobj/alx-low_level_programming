906ad864af8f59377777e2f598e6a4552aa3d3af
kowusubamfo <kowusubamfo@gmail.com>
2023-03-14 23:32:37 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,3 +8,21 @@
  */
 
 
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+    s = malloc(sizeof(char) * size);
+
+	if (size == 0 || s == NULL)
+		return (NULL);
+
+    i = 0;
+
+    while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
+}
