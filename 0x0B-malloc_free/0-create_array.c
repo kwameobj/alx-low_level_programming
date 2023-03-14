348c25857b4ef8dc66007de64c020f611fc26deb
kowusubamfo <kowusubamfo@gmail.com>
2023-03-14 23:36:25 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,22 +4,24 @@
 /**
  * create_array - creates an array of chars,
  * and initializes it with a specific char
+ * @size: size of array
+ * @c: char given
  * Return: 0
  */
-
 
 char *create_array(unsigned int size, char c)
 {
 	char *s;
 	unsigned int i;
-    s = malloc(sizeof(char) * size);
+
+	s = malloc(sizeof(char) * size);
 
 	if (size == 0 || s == NULL)
 		return (NULL);
 
-    i = 0;
+	i = 0;
 
-    while (i < size)
+	while (i < size)
 	{
 		s[i] = c;
 		i++;
