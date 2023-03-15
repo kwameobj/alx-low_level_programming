8140e60d699edcec1cfe9c45f46cb248da9dda9a
kowusubamfo <kowusubamfo@gmail.com>
2023-03-15 00:51:48 +0000

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,25 +2,27 @@
 #include "main.h"
 
 /**
- * cacdc
+ * _strdup - returns a pointer to a new string
+ * which is a duplicate of another string
+ * @str: given string array
+ * Return: new string
 */
 
 char *_strdup(char *str)
-
 {
-    char *dupli_str;
+	char *dupli_str;
 	int i;
-    int j = 0;
+	int j = 0;
 
-    dupli_str = malloc(sizeof(char) * j + 1);
+	dupli_str = malloc(sizeof(char) * j + 1);
 
-    if (str == NULL || dupli_str == NULL)
-        return(NULL)
+	if (str == NULL || dupli_str == NULL)
+		return (NULL);
 
-    for (i = 0; str[i]; i++)
-        dupli_str[i] = str[i];
+	for (i = 0; str[i]; i++)
+		dupli_str[i] = str[i];
 
-    dupli_str[j] = '\0'
+	dupli_str[j] = '\0';
 
-    return(dupli_str);
+	return (dupli_str);
 }
