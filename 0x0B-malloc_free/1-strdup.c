597589609b7d9e488030cb4a5623aaadf2b356ae
kowusubamfo <kowusubamfo@gmail.com>
2023-03-15 00:41:10 +0000

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * cacdc
+*/
+
+char *_strdup(char *str)
+
+{
+    char *dupli_str;
+	int i;
+    int j = 0;
+
+    dupli_str = malloc(sizeof(char) * j + 1);
+
+    if (str == NULL || dupli_str == NULL)
+        return(NULL)
+
+    for (i = 0; str[i]; i++)
+        dupli_str[i] = str[i];
+
+    dupli_str[j] = '\0'
+
+    return(dupli_str);
+}
