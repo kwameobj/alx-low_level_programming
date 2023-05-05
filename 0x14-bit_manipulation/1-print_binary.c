7496d64a169895a2f7125fbc802c74933c352ca3
kowusubamfo <kowusubamfo@gmail.com>
2023-05-05 05:43:34 +0000

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary: prints the binary representation of a given number
+ * @n: the given number
+ *
+ * Return: 0
+ */
+
+void print_binary(unsigned long int n);
+{
+	int i, j;
+
+	if (n == 0)
+		_putchar('0');
+
+	for (j = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+			j = 1;
+		if (j == 1)
+			((n >> i) & 1) ? _putchar('1') : _putchar('0');
+	}
+}
