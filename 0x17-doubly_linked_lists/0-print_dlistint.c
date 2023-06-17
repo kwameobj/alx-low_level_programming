9225b7620334fa91d34cced66e300179f774813b
kwameobj <kowusubamfo@gmail.com>
2023-06-16 18:50:56 -0700

    Added task zero files

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
  *
  * @h: head of the list
  * Return: the number of nodes
  */
-
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+	int i;
 
-	count = 0;
+	i = 0;
 
 	if (h == NULL)
-		return (count);
+		return (i);
 
 	while (h->prev != NULL)
 		h = h->prev;
@@ -22,9 +22,9 @@ size_t print_dlistint(const dlistint_t *h);
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
+		i++;
 		h = h->next;
 	}
 
-	return (count);
+	return (i);
 }
