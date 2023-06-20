72969ce05cbb8c67988c5418370709a1f2fc5f06
kwameobj <kowusubamfo@gmail.com>
2023-06-20 02:40:43 +0000

    Trying task 3

---
+++ 0x18-dynamic_libraries/101-win_code.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * rand - generates a sequnce of pseudo random numbers
+ *
+ * Return: 0
+ */
+
+int rand(void)
+{
+	static int num = -1;
+
+	num++;
+	if (num == 0)
+		return (8);
+	if (num == 1)
+		return (8);
+	if (num == 2)
+		return (7);
+	if (num == 3)
+		return (9);
+	if (num == 4)
+		return (23);
+	if (num == 5)
+		return (74);
+	return (num * num % 30000);
+}
