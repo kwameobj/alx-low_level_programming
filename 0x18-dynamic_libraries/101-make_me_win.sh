cfd020460664a37eddc62d94f5ecf1a3c8de74fb
kwameobj <kowusubamfo@gmail.com>
2023-06-20 02:11:14 +0000

    Trying task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD=/usr/local/my_lib
-./my_lib
+curl -o winlib.so https://github.com/kwameobj/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libdynamic.so
+export LD_PRELOAD=/usr/local/winlib.so
