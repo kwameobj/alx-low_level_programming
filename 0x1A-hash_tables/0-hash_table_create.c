869bfa57515c333f877aeac04283187e58b017ec
kwameobj <kowusubamfo@gmail.com>
2023-07-07 11:53:12 +0000

    Updated Task 0 file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 {
 	hash_table_t *ht;
+	unsigned int i;
 
 	if (size == 0)
 	return (NULL);
@@ -25,5 +26,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(ht);
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+		ht -> array[i] = NULL;
 	return (ht);
 }
