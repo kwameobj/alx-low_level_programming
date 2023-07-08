d70dce20a7b3fc9942d6376777adc628486c0478
kwameobj <kowusubamfo@gmail.com>
2023-07-08 00:36:12 +0000

    Updated Task 6 file

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,12 +9,12 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node, *temp;
-	unsigned long int cont = 0;
+	unsigned long int i = 0;
 
-	for (cont = 0; cont < ht->size; cont++)
+	for (i = 0; i < ht->size; i++)
 	{
-		node = ht->array[cont];
-		while (node != NULL)
+		node = ht->array[i];
+		while (node)
 		{
 			temp = node;
 			node = node->next;
